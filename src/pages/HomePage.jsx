d0d6ca0679a9bc0b82b6e6749dea76176bd484af
Easonlu0425 <andy1234532@hotmail.com>
2023-09-22 22:08:20 +0800

    feat: add homepage template

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,23 @@
 import { Navbar } from "../components";
 import styles from './HomePage.module.css'
+import { Link, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <>
       <Navbar></Navbar>
       <div className={styles.homePageContainer}>
-        <div>Home Page</div>
+        <div className={styles.title}>
+          <h3>我的行程</h3>
+        </div>
+        <div className={styles.listContainer}>
+          <div className={styles.group}>
+            <Link to="/">
+              <div className={styles.groupName}>Group Title</div>
+              <div className={styles.groupsMembers}>成員:</div>
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   );
