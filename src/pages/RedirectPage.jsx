0ffa1aabf71e6e4691844420a9596aa61764e75c
Easonlu0425 <andy1234532@hotmail.com>
2023-10-24 16:50:51 +0800

    modify redirectPage

--- src/pages/RedirectPage.jsx
+++ src/pages/RedirectPage.jsx
@@ -1,17 +1,18 @@
-
+import { useAuth } from "contexts/AuthContext";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const RedirectPage = () => {
   const navigate = useNavigate();
+  const { isAuthenticated } = useAuth();
   useEffect(() => {
-    // if (isAuthenticated) {
-    //   navigate("/todos");
-    // } else {
-    //   navigate("/login");
-    // }
-    navigate('/login')
-  }, [navigate])
-}
+    if (isAuthenticated) {
+      navigate("/todos");
+    } else {
+      navigate("/login");
+    }
+    navigate("/login");
+  }, [navigate]);
+};
 
-export default RedirectPage
+export default RedirectPage;
